951be7b1a445a5f1ac0053005a6a6498b936ba1f
Luana Manhabusque <luanamanhabusque@outlook.com>
2021-12-07 00:03:05 -0300

    06

--- App.js
+++ App.js
@@ -7,28 +7,28 @@ import { Button, View } from 'react-native';
 //import UserContextProvider from './src/Contexts/userContext';
 //import { StyleSheet } from 'react-native';
 
-export default function SignIn() {
+function Preload({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go"
+        onPress={() => navigation.navigate('Preload')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
     <NavigationContainer>
-     <div class="w-96">
-    <div class="relative bg-green-500 bg-opacity-80" style="width: 375px; height: 667px;">
-        <p class="w-36 h-16 absolute text-7xl text-green-900" style="left: 115px; top: 201px;">iPed</p>
-        <img class="w-28 h-24 absolute rounded-lg" style="left: 134px; top: 317px;" src="https://via.placeholder.com/106x89"/>
-        <img class="w-36 h-36 absolute" style="left: 108px; top: 286px;" src="https://via.placeholder.com/152x150"/>
-    </div>
-</div>
+      <Stack.Navigator initialRouteName="Preload">
+        <Stack.Screen name="Preload" component={Preload} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-
-}
-function MyStack() {
-
-  const Stack = createNativeStackNavigator();
-
-  return (
-      <Stack.Navigator>
-        <Stack.Screen> name="SignIn" component{Preload}</Stack.Screen>
-      </Stack.Navigator>
-  ); 
 }
 
+export default App;
